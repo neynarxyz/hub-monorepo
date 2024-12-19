383e8ec756a8d9284e03fdf9f099944191d4899f
Ty Book <ty@neynar.com>
2024-12-19 14:33:50 -0500

    Baseline changes for subrepo Shuttle to work with Neynar's Yarn workspace setup

--- packages/shuttle/src/example-app/app.ts
+++ packages/shuttle/src/example-app/app.ts
@@ -99,7 +99,7 @@ export class App implements MessageHandler {
       redis,
       shardKey,
       log,
-      null,
+      undefined,
       totalShards,
       shardIndex,
       SUBSCRIBE_RPC_TIMEOUT,
@@ -247,6 +247,10 @@ export class App implements MessageHandler {
 
   async backfillFids(fids: number[], backfillQueue: Queue) {
     const startedAt = Date.now();
+    if (!this.hubSubscriber.hubClient) {
+      log.error("Hub client is not initialized");
+      throw new Error("Hub client is not initialized");
+    }
     if (fids.length === 0) {
       let maxFid = MAX_FID ? parseInt(MAX_FID) : undefined;
       if (!maxFid) {
