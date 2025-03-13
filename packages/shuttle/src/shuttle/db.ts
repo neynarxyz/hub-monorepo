c638d0f0bccce5e4a7722c48355e65c3803941de
lucas-neynar <lucas@neynar.com>
2025-03-12 18:17:45 -0700

    feat: add hub event reconciler [NEYN-2651] (#2)
    * feat: add hub event reconciler
    
    * refactor: rename hubEventReconciliation to onChainEventReconciliation
    
    * fix: add event body types
    
    * fix: update logs and errors
    
    * fix: use built-in error method and fix build error with example-app
    
    * fix: restore example-app
    
    * fix: remove test code
    
    * fix: update processHubEvent logic
    
    * fix: type error
    
    * fix: remove empty file
    
    * feat: update onChainEventReconciliation to use maps
    
    * Update packages/shuttle/src/shuttle/onChainEventReconciliation.ts
    
    Co-authored-by: Ty Book <tbook11@gmail.com>
    
    * fix: rename functions and remove unnecessary logic
    
    * fix: move onchain_events table to HubTables
    
    * fix: Onchain to OnChain
    
    * fix: rename more things to onChain pattern, add support for SignerMigrated event, and fix example insert
    
    * fix: remove unused import
    
    * refactor: rename to onChain pattern
    
    * fix: update date comparison logic
    
    * fix: set fid to zero for signer migrated events
    
    * Revert "fix: set fid to zero for signer migrated events"
    
    This reverts commit d21fdbb6814aaa66b7edf50f18510e42655628bc.
    
    * fix: set fid to zero for signer migrated event lookups
    
    * fix: set fid to zero for signer migrated events before hubble queries
    
    ---------
    
    Co-authored-by: Ty Book <tbook11@gmail.com>

--- packages/shuttle/src/shuttle/db.ts
+++ packages/shuttle/src/shuttle/db.ts
@@ -24,6 +24,11 @@ import {
   SignatureScheme,
   UserDataType,
   UserNameType,
+  OnChainEventType,
+  IdRegisterEventBody,
+  SignerEventBody,
+  StorageRentEventBody,
+  SignerMigratedEventBody,
 } from "@farcaster/hub-nodejs";
 import { DrainOuterGeneric, SimplifySingleResult } from "kysely/dist/cjs/util/type-utils.js";
 
@@ -140,9 +145,27 @@ type MessagesTable = {
 export type MessageRow = Selectable<MessagesTable>;
 export type InsertableMessageRow = Insertable<MessagesTable>;
 
+export type OnChainEventsTable = {
+  id: Generated<string>;
+  chainId: bigint;
+  createdAt: Generated<Date>;
+  updatedAt: Generated<Date>;
+  blockTimestamp: Date;
+  blockNumber: bigint;
+  logIndex: number;
+  txHash: Uint8Array;
+  type: OnChainEventType;
+  fid: Fid;
+  body: IdRegisterEventBody | SignerEventBody | StorageRentEventBody | SignerMigratedEventBody;
+};
+
+export type OnChainEventRow = Selectable<OnChainEventsTable>;
+export type InsertableOnChainEventRow = Insertable<OnChainEventsTable>;
+
 // ALL TABLES -------------------------------------------------------------------------------------
 export interface HubTables {
   messages: MessagesTable;
+  onchain_events: OnChainEventsTable;
 }
 
 export const getDbClient = (connectionString?: string, schema = "public") => {
